36bf8c945cc2d6b9a41e113832b693f47698290d
KonnaEll <konstadina.ell@gmail.com>
2025-05-13 15:36:49 +0200

    test

--- main.py
+++ main.py
@@ -4,4 +4,3 @@ from datasets import load_dataset
 
 # Load the ArXiv dataset (you can specify the field/topic if necessary)
 dataset = load_dataset("arxiv", split="train[:10%]")  # For example, using a subset
-print(dataset)
