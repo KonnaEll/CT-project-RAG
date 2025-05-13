7f906dda2cb73c87280344b5165c4218f1593960
KonnaEll <konstadina.ell@gmail.com>
2025-05-13 15:19:39 +0200

    add branch for konna

---
+++ main.py
@@ -0,0 +1,7 @@
+from torchvision import datasets
+
+from datasets import load_dataset
+
+# Load the ArXiv dataset (you can specify the field/topic if necessary)
+dataset = load_dataset("arxiv", split="train[:10%]")  # For example, using a subset
+print(dataset)
